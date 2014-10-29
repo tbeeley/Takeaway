01bbc0e82d7f01c25c75fa293b09af41ef9f983b
Thomas Beeley <tbeeley@gmail.com>
2014-10-29 10:40:34 +0000

    user.rb edited

--- lib/user.rb
+++ lib/user.rb
@@ -12,11 +12,15 @@ class User
 	def check_number(number)
 		if number[0].to_i != 0
 			raise 'That number is not real'
-		elsif number.length > 11
+		elsif number_length > 11
 			raise 'That number is too long'
-		elsif number.length < 11
+		elsif number_length < 11
 			raise 'That number is too short'
 		end
+	end
+
+	def number_length
+		number.length
 	end
 
 	def make_order(order)
